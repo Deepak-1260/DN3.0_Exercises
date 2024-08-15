02b92b644e9e7e4d2dd4eddb427982e2b4a7dab7
Kella Deepak Kumar <114850599+Deepak-1260@users.noreply.github.com>
2024-08-15 19:24:25 +0530

    Delete Week3_Exercise/EmployeeManagementSystem

--- Week3_Exercise/EmployeeManagementSystem/1-10 Exercises/src/main/java/com/example/EmployeeManagementSystem/projection/DepartmentProjection.java
+++
@@ -1,6 +0,0 @@
-package com.example.EmployeeManagementSystem.projection;
-
-public interface DepartmentProjection {
-    Long getId();
-    String getName();
-}
--- Week3_Exercise/EmployeeManagementSystem/1-10 Exercises/src/main/java/com/example/EmployeeManagementSystem/projection/EmployeeProjection.java
+++
@@ -1,7 +0,0 @@
-package com.example.EmployeeManagementSystem.projection;
-
-public interface EmployeeProjection {
-    Long getId();
-    String getName();
-    String getEmail();
-}
